63178464cdcd6a47e4568640324d51c524d9342d
Alessio Franceschelli <alessio.franceschelli@newday.co.uk>
2023-12-12 17:08:54 +0000

    DI registrations fix and Functions v4 sample (#53)

---
+++ sample/SampleFunctionApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+        {
+            var options = new OpenApiConfigurationOptions()
+            {
+                Info = new OpenApiInfo()
+                {
+                    Version = DefaultOpenApiConfigurationOptions.GetOpenApiDocVersion(),
+                    Title = $"{DefaultOpenApiConfigurationOptions.GetOpenApiDocTitle()}",
+                    Description = DefaultOpenApiConfigurationOptions.GetOpenApiDocDescription(),
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "sample",
+                        Email = "test@example.com",
+                        Url = new Uri("https://github.com/NewDayTechnology/NewDay.Extensions.FunctionsValidationFilter"),
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "Apache-2.0",
+                        Url = new Uri("https://opensource.org/license/apache-2-0/"),
+                    }
+                },
+                Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                OpenApiVersion = DefaultOpenApiConfigurationOptions.GetOpenApiVersion(),
+                IncludeRequestingHostName = DefaultOpenApiConfigurationOptions.IsFunctionsRuntimeEnvironmentDevelopment(),
+                ForceHttps = DefaultOpenApiConfigurationOptions.IsHttpsForced(),
+                ForceHttp = DefaultOpenApiConfigurationOptions.IsHttpForced(),
+            };
+
+            options.DocumentFilters.AddFunctionsValidationFilter<Program>();
+
+            return options;
+        });
+        services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+    })
+    .Build();
+
+host.Run();
---
+++ sample/SampleFunctionApp/SampleFunction.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace SampleFunctionApp
+{
+    public class SampleFunction
+    {
+        private readonly ILogger _logger;
+        private readonly IValidator<SampleInput> _validator;
+
+        public SampleFunction(ILoggerFactory loggerFactory, IValidator<SampleInput> validator)
+        {
+            _logger = loggerFactory.CreateLogger<SampleFunction>();
+            _validator = validator;
+        }
+
+        [Function("SampleFunction")]
+        [OpenApiOperation(operationId: "sampleFunction", tags: new[] { "sample" }, Summary = "Sample input validation", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(SampleInput), Required = true)]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(ProblemDetails), Summary = "Invalid input supplied")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var input = await req.ReadFromJsonAsync<SampleInput>();
+            if (input is null)
+            {
+                var error = req.CreateResponse(HttpStatusCode.BadRequest);
+                await error.WriteAsJsonAsync(
+                    new ProblemDetails
+                    {
+                        Status = (int)HttpStatusCode.BadRequest,
+                        Title = "Missing body"
+                    });
+
+                return error;
+            }
+
+            var validationResult = await _validator.ValidateAsync(input);
+            if (!validationResult.IsValid)
+            {
+                var error = req.CreateResponse(HttpStatusCode.BadRequest);
+                await error.WriteAsJsonAsync(
+                    new ProblemDetails
+                    {
+                        Status = (int)HttpStatusCode.BadRequest,
+                        Title = "One or more validation errors occurred.",
+                        Extensions = {
+                            ["errors"] = validationResult.Errors.Select(e => new
+                                {
+                                    e.ErrorCode,
+                                    e.PropertyName,
+                                    e.ErrorMessage
+                                })
+                        }
+                    });
+
+                return error;
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
